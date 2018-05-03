b7b4f62d0ba049f00a8b37a5851c53994ac0701e
Alec Hunter <37111157+alik250@users.noreply.github.com>
2018-05-03 04:16:22 -0400

    Updated to kotlin 1.2.41 Readability changes and other misc things

--- app/src/main/java/com/alce/repeeder/MainActivity.kt
+++ app/src/main/java/com/alce/repeeder/MainActivity.kt
@@ -3,9 +3,8 @@ package com.alce.repeeder
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,12 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val incorrectTextToast = Toast.makeText(this, "No text entered", Toast.LENGTH_SHORT)
-        val incorrectNumToast = Toast.makeText(this, "Incompatible number value entered", Toast.LENGTH_SHORT)
-        val resultErrorToast = Toast.makeText(this, "Error occurred creating output", Toast.LENGTH_LONG)
-        val copiedToClipboardToast = Toast.makeText(this, "Copied to clipboard!", Toast.LENGTH_SHORT)
-        val notCopiedToClipboardToast = Toast.makeText(this, "Error copying to clipboard. Try copying manually. ", Toast.LENGTH_LONG)
-
         val textToBeRepeated = findViewById<TextView>(R.id.textToBeRepeated)
         val numberOfTimesToRepeat = findViewById<TextView>(R.id.numberTimesToRepeat)
         val resultsView = findViewById<TextView>(R.id.resultsView)
@@ -29,24 +22,44 @@ class MainActivity : AppCompatActivity() {
         val repeatButton = findViewById<Button>(R.id.repeatTextButton)
         val copyToClipboardButton = findViewById<Button>(R.id.copyToClipboardButton)
 
+        var previousText: CharSequence = ""
+        var previousNum: CharSequence = ""
 
         repeatButton.setOnClickListener {
-            if (textToBeRepeated.text.isEmpty()) {
-                incorrectTextToast.show()
-            } else if (numberOfTimesToRepeat.text.isEmpty() || numberOfTimesToRepeat.text.toString().toInt() <= 0) {
-                incorrectNumToast.show()
-            } else {
-                try {
+
+            if (textToBeRepeated.text.isEmpty())
+            {
+                showToast(getString(R.string.Toast_Error_NoText))
+            }
+            else if (numberOfTimesToRepeat.text.isEmpty() || numberOfTimesToRepeat.text.toString().toInt() <= 0)
+            {
+                showToast(getString(R.string.Toast_Error_WrongNumber))
+            }
+            else if ((textToBeRepeated.text == previousText) && (numberOfTimesToRepeat.text == previousNum))
+            {
+                println("Debug: Nothing happened.")
+                showToast("Nothing happened")
+            }
+            else
+            {
+                try
+                {
                     resultsView.text = repeatText(
                             textToBeRepeated.text.toString(),
                             numberOfTimesToRepeat.text.toString().toInt()
                     )
-                } catch (e: Exception) {
+                } catch (e: Exception)
+                {
                     println("Exception trying to create string. Perhaps the string is too long? ")
                     println(e)
-                    resultErrorToast.show()
+                    showToast(getString(R.string.Toast_Error_ResultError), false)
                 }
             }
+            println(previousText)
+            println(previousNum)
+            previousText = textToBeRepeated.text
+            previousNum = numberOfTimesToRepeat.text
+
         }
 
 
@@ -58,26 +71,37 @@ class MainActivity : AppCompatActivity() {
                     resultsView.text
             )
 
-            clipboard.primaryClip = clip
+            for (i in 1..1) {
+                if (resultsView.text.isEmpty()) {
+                    showToast(getString(R.string.Toast_Warning_NothingToCopy))
+                    continue                                                        //Don't want to overwrite the clipboard with an empty clip
+                }
 
-            if (clipboard.primaryClip.getItemAt(0).text.toString() == resultsView.text.toString()) {
-                copiedToClipboardToast.show()
-            } else {
-                notCopiedToClipboardToast.show()
-                println("on clipboard: " + clipboard.primaryClip.getItemAt(0).text)
-                println("resultsView:  " + resultsView.text)
+                clipboard.primaryClip = clip
+
+                if (clipboard.primaryClip.getItemAt(0).text.toString() == resultsView.text.toString()) {
+                    showToast(getString(R.string.Toast_Success_CopiedToClipboard), false)
+                } else {
+                    showToast(getString(R.string.Toast_Error_FailedCopyingToClipboard))
+                    println("on clipboard: " + clipboard.primaryClip.getItemAt(0).text)
+                    println("resultsView:  " + resultsView.text)
+                }
             }
         }
     }
 
+    // Custom functions here
 
-    fun repeatText(string: String, numberOfTimes: Int): String {
+    private fun repeatText(string: String, numberOfTimes: Int): String {        //Primary logic
         var output = string
-        for (i in 1..numberOfTimes - 1) {
+        for (i in 1 until numberOfTimes) {
             output += string
         }
         return output
     }
 
 
+    private fun showToast(string: String, trueIfShort: Boolean = true){         //Generates Toasts on the fly
+        Toast.makeText(this, string, if (trueIfShort) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
+    }
 }
